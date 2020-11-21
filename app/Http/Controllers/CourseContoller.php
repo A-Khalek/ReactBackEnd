969b2b7344962cf4240fb6235a21ded48911192e
A-Khalek <khalek.raju.bd@gmail.com>
2020-11-21 21:34:44 +0600

    Course controller method
    add required method for course controller , and router in web.php

--- app/Http/Controllers/CourseContoller.php
+++ app/Http/Controllers/CourseContoller.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\CourseTableModel;
 
 class CourseContoller extends Controller
 {
-    //
+
+    function onSelectFour(){
+        $result = CourseTableModel::limit(4)->get();
+        return $result;
+    }
+
+    function onSelectAll(){
+        $result = CourseTableModel::All();
+        return $result;
+    }
+
+    function onSelectDetails(Request $req){ 
+        
+        $id = $req->input('id');
+        $result = CourseTableModel::where(['id'=>$id]) ->get();
+        return $result;
+    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@
 $router->get('/ChartData', 'ChartDataController@onAllSelect');
 $router->get('/ClientReview','ClientReviewController@onAllSelect');
 $router->post('/ContactData','ContactController@onContactSend');
+
+$router->get('/CourseHome','CourseContoller@onSelectFour');
+$router->get('/CourseAll','CourseContoller@onSelectAll');
+$router->post('/CourseDetails','CourseContoller@onSelectDetails');
