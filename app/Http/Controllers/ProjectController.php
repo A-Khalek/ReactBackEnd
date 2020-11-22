ea1bc5060c7c0ee7ee94afd44efbe76f18d766b2
A-Khalek <khalek.raju.bd@gmail.com>
2020-11-23 00:49:44 +0600

    add function in projectController

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\ProjectModel;
 
 class ProjectController extends Controller
 {
-    //
+    function onSelect3(){
+        $result =  ProjectModel::limiti(3)->get();
+        return $result;
+    }
+
+    function onSelectAll(){
+        $result =  ProjectModel::All();
+        return $result;
+    }
+
+    function onSelectDetails(Request $req){
+        $id = $req->input($id);
+        $result = ProjectModel::where(['$req'=>$id])->get();
+        return $result;
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,7 @@ $router->post('/CourseDetails','CourseContoller@onSelectDetails');
 
 $router->get('/footerDetails','FooterController@onSelectAll');
 $router->get('/serviceDetails','ServiceController@onSelectAllService');
+
+$router->get('/project3','ProjectController@onSelect3');
+$router->get('/projectSelectAll','ProjectController@onSelectAll');
+$router->post('/projectDetails','ProjectController@onSelectDetails');
