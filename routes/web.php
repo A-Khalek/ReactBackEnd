c340cbf6a2d9f76451545d218e67ff31a2ed28c3
A-Khalek <khalek.raju.bd@gmail.com>
2020-11-24 21:53:56 +0600

    middleware auth
    add middleware auth in web.php

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,24 @@
 
 
 
-$router->get('/ChartData', 'ChartDataController@onAllSelect');
-$router->get('/ClientReview','ClientReviewController@onAllSelect');
-$router->post('/ContactData','ContactController@onContactSend');
+$router->get('/ChartData', ['middleware'=>'auth','uses'=>'ChartDataController@onAllSelect']);
+$router->get('/ClientReview',['middleware'=>'auth','uses'=>'ClientReviewController@onAllSelect']);
+$router->post('/ContactData',['middleware'=>'auth','uses'=>'ContactController@onContactSend']);
 
-$router->get('/CourseHome','CourseContoller@onSelectFour');
-$router->get('/CourseAll','CourseContoller@onSelectAll');
-$router->post('/CourseDetails','CourseContoller@onSelectDetails');
+$router->get('/CourseHome',['middleware'=>'auth','uses'=>'CourseContoller@onSelectFour']);
+$router->get('/CourseAll',['middleware'=>'auth','uses'=>'CourseContoller@onSelectAll']);
+$router->post('/CourseDetails',['middleware'=>'auth','uses'=>'CourseContoller@onSelectDetails']);
 
-$router->get('/footerDetails','FooterController@onSelectAll');
-$router->get('/serviceDetails','ServiceController@onSelectAllService');
+$router->get('/footerDetails',['middleware'=>'auth','uses'=>'FooterController@onSelectAll']);
+$router->get('/serviceDetails',['middleware'=>'auth','uses'=>'ServiceController@onSelectAllService']);
 
-$router->get('/project3','ProjectController@onSelect3');
-$router->get('/projectSelectAll','ProjectController@onSelectAll');
-$router->post('/projectDetails','ProjectController@onSelectDetails');
+$router->get('/project3',['middleware'=>'auth','uses'=>'ProjectController@onSelect3']);
+$router->get('/projectSelectAll',['middleware'=>'auth','uses'=>'ProjectController@onSelectAll']);
+$router->post('/projectDetails',['middleware'=>'auth','uses'=>'ProjectController@onSelectDetails']);
 
-$router->get('/videoHome','HomeController@onVideotAll');
-$router->get('/totalProjectClient','HomeController@onSelectClientProject');
-$router->get('/tectDesc','HomeController@onSelectTechDesc');
-$router->get('/homeTitle','HomeController@onSelectHomeTitle');
+$router->get('/videoHome',['middleware'=>'auth','uses'=>'HomeController@onVideotAll']);
+$router->get('/totalProjectClient',['middleware'=>'auth','uses'=>'HomeController@onSelectClientProject']);
+
+
+$router->get('/tectDesc',['middleware'=>'auth','uses'=>'HomeController@onSelectTechDesc']);
+$router->get('/homeTitle',['middleware'=>'auth','uses'=>'HomeController@onSelectHomeTitle']);
